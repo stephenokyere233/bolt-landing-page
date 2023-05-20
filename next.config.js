7c9b5912a6521de93246b9e1e21cce49827f4cfc
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2023-05-20 17:07:33 +0000

    [add] update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{
+  images: {
     domains: ["bolt.eu"]
-  }
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
